2d625ed0d4aa95a726a21ff3280c9409169bec39
Andrea Alberti <a.alberti82@gmail.com>
2021-06-20 00:34:20 +0200

    Initial Commit

---
+++ VolumeControl/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  VolumeControl
+//
+//  Created by Andrea Alberti on 20.06.21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ VolumeControl/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  VolumeControl
+//
+//  Created by Andrea Alberti on 20.06.21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
